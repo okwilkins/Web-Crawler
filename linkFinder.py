5c3d2905e1c7f6afa7ba38c5ff742b11b13529f5
Oliver Wilkins <okwilkins@googlemail.com>
2018-03-19 21:22:38 +0000

    Add: Link Parser Class
    Adds onto the inbuilt HTMLParser lib. This allows links to be found from a given URL.

---
+++ linkFinder.py
@@ -0,0 +1,26 @@
+from html.parser import HTMLParser
+from urllib import parse
+
+class LinkFinder(HTMLParser):
+
+
+	def __init__(self, baseURL, pageURL):
+		super().__init__()
+		self.baseURL = baseURL
+		self.pageURL = pageURL
+		self.links = set()
+
+	def handleStartTag(self, tag, attrs):
+		if tag == 'a':
+			for (attribute, value) in attrs:
+				if attribute == 'href':
+					url = parse.urljoin(self.baseURL, value)
+					self.links.add(url)
+
+	def pageLinks(self):
+		return self.links
+
+	def error(self, message):
+		pass
+
+finder = LinkFinder()
